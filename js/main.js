c5f922823c5a88ca05d0475014025cf2d0a2fbe9
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-02 11:27:11 +0530

    Location Input Working

--- js/main.js
+++ js/main.js
@@ -1,56 +1,81 @@
 let weatherDisplay = document.querySelector(".display-weather");
 let feelsLike = document.querySelector(".feels-like");
 let weatherIcon = document.querySelector(".weather-icon");
+let userLocation = document.querySelector(".user-location");
+let getLocationBtn = document.querySelector(".get-locatiion-btn");
+let weatherDetailsContainer = document.querySelector(".weather-details");
+let currentLocation = document.querySelector(".location-current");
+let inputContainer = document.querySelector(".location-input-area");
 
 const getWeather = async () => {
-  const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no"
-  ).then((response) => {
+  let location =
+    "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
+    userLocation.value +
+    "&aqi=no";
+  console.log(location);
+
+  // "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=Kolkata&aqi=no";
+  const response = await fetch(location).then((response) => {
     return response.json();
   });
   console.log(response);
   return response;
 };
 
-let weatherData = getWeather().then((response) => {
-  console.log(response);
-  console.log("Response : " + response);
-  let weatherCondition = response.current.condition.text;
-  console.log(weatherCondition);
+const getWeatherinfo = () => {
+  inputContainer.classList.remove("location-input-area");
+  weatherDetailsContainer.classList.remove("visually-hidden");
+  currentLocation.innerHTML = userLocation.value;
+  getWeather().then((response) => {
+    console.log(response);
 
-  if (weatherCondition == "Overcast") {
-    weatherIcon.removeAttribute("class");
-    weatherIcon.classList.add("fa", "fa-cloud", "my-auto", "weather-icon");
-    // weatherIcon.classList.add("fa-cloud");
-    // weatherIcon.classList.add("my-auto");
-    // weatherIcon.classList.add("fs-1");
-    // weatherIcon.classList.add("weather-icon");
-  } else if (weatherCondition == "Sunny") {
-    weatherIcon.removeAttribute("class");
-    weatherIcon.classList.add("fa", "fa-sun", "my-auto", "weather-icon");
-  } else if (weatherCondition == "Rainy") {
-    weatherIcon.removeAttribute("class");
-    weatherIcon.classList.add("fa", "fa-rain", "my-auto", "weather-icon");
-  } else if (weatherCondition == "Partly cloudy") {
-    weatherIcon.removeAttribute("class");
-    weatherIcon.classList.add(
-      "fa",
-      "fa-cloud-sun",
-      "my-auto",
-      "weather-icon",
-      "text-"
-    );
-  } else {
-    console.log("called!");
-    weatherIcon.removeAttribute("class");
-    weatherIcon.classList.add(
-      "fa",
-      "fa-sun",
-      "my-auto",
-      "weather-icon",
-      "text-warning"
-    );
-  }
-  weatherDisplay.textContent = response.current.temp_f;
-  feelsLike.textContent = "Feels Like " + response.current.feelslike_f;
-});
+    let weatherCondition = response.current.condition.text;
+    console.log(weatherCondition);
+
+    if (weatherCondition == "Overcast") {
+      weatherIcon.removeAttribute("class");
+      weatherIcon.classList.add("fa", "fa-cloud", "my-auto", "weather-icon");
+    } else if (weatherCondition == "Sunny") {
+      weatherIcon.removeAttribute("class");
+      weatherIcon.classList.add("fa", "fa-sun", "my-auto", "weather-icon");
+    } else if (weatherCondition == "Rainy") {
+      weatherIcon.removeAttribute("class");
+      weatherIcon.classList.add(
+        "fa",
+        "fa-cloud-rain",
+        "my-auto",
+        "weather-icon"
+      );
+    } else if (weatherCondition == "Partly cloudy") {
+      weatherIcon.removeAttribute("class");
+      weatherIcon.classList.add(
+        "fa",
+        "fa-cloud-sun",
+        "my-auto",
+        "weather-icon"
+      );
+    } else if (weatherCondition == "Clear") {
+      console.log("called!");
+      weatherIcon.removeAttribute("class");
+      weatherIcon.classList.add(
+        "fa",
+        "fa-sun",
+        "my-auto",
+        "weather-icon",
+        "text-warning"
+      );
+    } else {
+      console.log("called!");
+      weatherIcon.removeAttribute("class");
+      weatherIcon.classList.add(
+        "fa",
+        "fa-spinner",
+        "fa-spin",
+        "my-auto",
+        "weather-icon"
+      );
+    }
+    weatherDisplay.textContent = response.current.temp_f;
+    feelsLike.textContent = "Feels Like " + response.current.feelslike_f;
+  });
+};
