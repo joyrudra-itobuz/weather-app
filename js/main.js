90a5f7373b118319013a342bd0af55548493022c
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-10 18:14:23 +0530

    Added Dropdown Funtionality

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const weatherDetailsContainer = document.querySelector(".weather-details");
 const currentLocation = document.querySelector(".location-current");
 const inputContainer = document.querySelector(".location-input-area");
 let currentWeatherIcon = document.querySelector(".current-weather-icon");
+const dropdownList = document.querySelector(".dropdown-list");
+const inputDropdown = document.querySelector(".input-dropdown");
+
+console.log(inputDropdown);
 
 const getWeather = async () => {
   let location =
@@ -16,57 +20,67 @@ const getWeather = async () => {
   console.log(location);
 
   // "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=Kolkata&aqi=no";
-  const response = await fetch(location).then((response) => {
-    return response.json();
-  });
+  const response = await fetch(location).then(
+    (response) => {
+      return response.json();
+    },
+    (err) => {
+      console.log(err);
+      return "Check Your Spelling or Server is busy!";
+    }
+  );
   console.log(response);
   return response;
 };
 
 const getWeatherinfo = () => {
   userInputLocation.value = userInputLocation.value;
-  getWeather().then((response) => {
-    console.log(response);
+  getWeather()
+    .then((response) => {
+      console.log(response);
 
-    let weatherCondition = response.current.condition.text;
-    console.log(weatherCondition);
+      let weatherCondition = response.current.condition.text;
+      console.log(weatherCondition);
 
-    if (weatherCondition == "Overcast") {
-      weatherIcon = document.querySelector(".overcast-icon");
-      weatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon = weatherIcon;
-    } else if (weatherCondition == "Sunny") {
-      weatherIcon = document.querySelector(".sunny-icon");
-      weatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon = weatherIcon;
-    } else if (weatherCondition == "Rainy") {
-      weatherIcon = document.querySelector(".cloudy");
-      weatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon = weatherIcon;
-    } else if (weatherCondition == "Partly cloudy") {
-      weatherIcon = document.querySelector(".cloudy-icon");
-      weatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon = weatherIcon;
-    } else if (weatherCondition == "Clear") {
-      weatherIcon = document.querySelector(".clear-icon");
-      weatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon = weatherIcon;
-    } else {
-      console.log("called!");
-      weatherIcon = document.querySelector(".clear-icon");
-      console.log(weatherIcon);
-      weatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon.classList.toggle("weather-icon-hidden");
-      currentWeatherIcon = weatherIcon;
-    }
-    mainTemp.textContent = response.current.temp_f;
-    tempFeelsLike.textContent = "Feels " + response.current.feelslike_f;
-  });
+      if (weatherCondition == "Overcast") {
+        weatherIcon = document.querySelector(".overcast-icon");
+        weatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon = weatherIcon;
+      } else if (weatherCondition == "Sunny") {
+        weatherIcon = document.querySelector(".sunny-icon");
+        weatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon = weatherIcon;
+      } else if (weatherCondition == "Rainy") {
+        weatherIcon = document.querySelector(".cloudy");
+        weatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon = weatherIcon;
+      } else if (weatherCondition == "Partly cloudy") {
+        weatherIcon = document.querySelector(".cloudy-icon");
+        weatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon = weatherIcon;
+      } else if (weatherCondition == "Clear") {
+        weatherIcon = document.querySelector(".clear-icon");
+        weatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon = weatherIcon;
+      } else {
+        console.log("called!");
+        weatherIcon = document.querySelector(".clear-icon");
+        console.log(weatherIcon);
+        weatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon.classList.toggle("weather-icon-hidden");
+        currentWeatherIcon = weatherIcon;
+      }
+      mainTemp.textContent = response.current.temp_f;
+      tempFeelsLike.textContent = "Feels " + response.current.feelslike_f;
+    })
+    .catch((err) => {
+      userInputLocation.value = err;
+    });
 };
 
 userInputLocation.addEventListener("keyup", function (e) {
@@ -74,3 +88,21 @@ userInputLocation.addEventListener("keyup", function (e) {
     getWeatherinfo();
   }
 });
+
+inputDropdown.addEventListener("click", () => {
+  dropdownList.classList.toggle("dropdown-list-toggle");
+});
+inputDropdown.addEventListener("blur", () => {
+  dropdownList.classList.toggle("dropdown-list-toggle");
+});
+
+// const getDropdownLocation = (userLocation) => {
+//   console.log(userInputLocation);
+//   userInputLocation.value = userLocation;
+// };
+
+function getDropdownLocation(userLocation) {
+  console.log("Called!");
+  userInputLocation.value = userLocation;
+  getWeatherinfo();
+}
